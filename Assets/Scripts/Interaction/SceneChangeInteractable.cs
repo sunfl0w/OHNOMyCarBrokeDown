0ad6791bb0977aeae8c6cacbf637f2fe41cedd70
sunfl0w <lucaanthonyschwarz@googlemail.com>
2024-01-08 23:24:18 +0100

    Fix scene transition

--- Assets/Scripts/Interaction/SceneChangeInteractable.cs
+++ Assets/Scripts/Interaction/SceneChangeInteractable.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 
-public class SceneChanceInteractable : MonoBehaviour, IInteractable
-{
+public class SceneChanceInteractable : MonoBehaviour, IInteractable {
     public string targetSceneName;
     public string targetTransformName;
     public InteractableData data;
@@ -13,42 +12,32 @@ public class SceneChanceInteractable : MonoBehaviour, IInteractable
     public TextMeshProUGUI hintText;
 
 
-    public void Interact()
-    {
-        if (PlayerInventory.Instance.itemExists(requiredItem))
-        {
+    public void Interact() {
+        if (PlayerInventory.Instance.itemExists(requiredItem) || requiredItem == null) {
             audioSource.PlayOneShot(data.interactSound);
             Debug.Log("Interactable. Switch to scene: " + targetSceneName);
             SceneTransitionManager.Instance.SetNextTransformByName(targetTransformName);
             StartCoroutine(Transition());
-        }
-        else
-        {
+        } else {
             hintText.text = "The door is locked...\n(Hint: Press [I] to toggle the inventory)";
             StartCoroutine(ClearHint());
-
         }
-
     }
 
-    public Transform GetTransform()
-    {
+    public Transform GetTransform() {
         return transform;
     }
 
-    public InteractableData GetData()
-    {
+    public InteractableData GetData() {
         return data;
     }
 
-    IEnumerator Transition()
-    {
+    IEnumerator Transition() {
         yield return new WaitForSeconds(3.0f);
         SceneManager.LoadScene(targetSceneName, LoadSceneMode.Single);
     }
 
-    IEnumerator ClearHint()
-    {
+    IEnumerator ClearHint() {
         yield return new WaitForSeconds(1.0f);
         hintText.text = "";
     }
