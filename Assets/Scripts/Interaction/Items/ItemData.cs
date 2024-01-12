f8a31126800aba3cc7131c15810d13b51b70ef48
sunfl0w <lucaanthonyschwarz@googlemail.com>
2024-01-12 13:54:41 +0100

    Added first car part collectible.

--- Assets/Scripts/Interaction/Items/ItemData.cs
+++ Assets/Scripts/Interaction/Items/ItemData.cs
@@ -1,23 +1,21 @@
 using UnityEngine;
 
-public enum ItemCategory
-{
+public enum ItemCategory {
     Weapon,
     Resource,
+    CarPart,
     Normal
 }
 
 // Simple item data representation as a scriptable object
 [CreateAssetMenu(fileName = "ItemData", menuName = "ScriptableObjects/ItemData", order = 1)]
-public class ItemData : ScriptableObject
-{
+public class ItemData : ScriptableObject {
     public string itemName;
     public string interactText;
     public GameObject prefab;
     public ItemCategory category;
 
-    public ItemData(string itemName, string interactText, GameObject prefab, ItemCategory category)
-    {
+    public ItemData(string itemName, string interactText, GameObject prefab, ItemCategory category) {
         this.itemName = itemName;
         this.interactText = interactText;
         this.prefab = prefab;
