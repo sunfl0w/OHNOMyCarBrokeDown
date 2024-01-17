1842a1721af7b21e3d4056f9b1c79f0dfed0885b
Citrus <koschei042@gmail.com>
2024-01-17 19:33:33 +0100

    add credits

--- Assets/Scripts/GUI/Menus/MainMenu.cs
+++ Assets/Scripts/GUI/Menus/MainMenu.cs
@@ -5,35 +5,53 @@ using UnityEngine.SceneManagement;
 using TMPro;
 using System;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
 
     public TextMeshProUGUI startGame;
     public TextMeshProUGUI credits;
     public TextMeshProUGUI quit;
-    public GameObject canvas;
+    public GameObject buttons;
+
+    public GameObject creditsContainer;
 
     private int selectedIndex = 0;
 
-    void Start() {
+    void Start()
+    {
         ChangeSelection(0);
     }
 
-    void Update() {
-        if (canvas.activeSelf) {
-            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) {
+    void Update()
+    {
+        if (buttons.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            {
                 ChangeSelection(-1);
-            } else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) {
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+            {
                 ChangeSelection(1);
             }
 
-            if (Input.GetKeyDown(KeyCode.Return)) {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
                 HandleSelection();
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                ToggleCredits();
             }
         }
 
     }
 
-    void ChangeSelection(int direction) {
+    void ChangeSelection(int direction)
+    {
         selectedIndex = (selectedIndex + direction + 3) % 3;
 
         startGame.color = (selectedIndex == 0) ? Color.red : Color.white;
@@ -41,26 +59,46 @@ public class MainMenu : MonoBehaviour {
         quit.color = (selectedIndex == 2) ? Color.red : Color.white;
     }
 
-    void HandleSelection() {
-        switch (selectedIndex) {
+    void HandleSelection()
+    {
+        switch (selectedIndex)
+        {
             case 0:
                 // Start Game and load last active scene
                 string currentSceneName = SavestateManager.Instance.GetCurrentSaveState().playerSaveState.currentSceneName;
-                if (currentSceneName != String.Empty) {
+                if (currentSceneName != String.Empty)
+                {
                     SceneTransitionManager.Instance.LoadScene(SavestateManager.Instance.GetCurrentSaveState().playerSaveState.currentSceneName);
-                } else {
+                }
+                else
+                {
                     SceneTransitionManager.Instance.LoadScene("OminousStreetScene");
                 }
                 break;
             case 1:
                 // Credits
                 Debug.Log("Credits selected");
+                ToggleCredits();
                 break;
             case 2:
                 // Quit
                 Debug.Log("Quit selected");
                 Application.Quit(); // Note: This will only work in a built application, not in the Unity Editor
                 break;
+        }
+    }
+
+    void ToggleCredits()
+    {
+        if (creditsContainer.activeSelf)
+        {
+            creditsContainer.SetActive(false);
+            buttons.SetActive(true);
+        }
+        else
+        {
+            creditsContainer.SetActive(true);
+            buttons.SetActive(false);
         }
     }
 
