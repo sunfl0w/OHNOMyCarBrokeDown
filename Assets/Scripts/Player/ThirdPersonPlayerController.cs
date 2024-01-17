4500f5d396abd3832fa8b20b962763ed9207e728
sunfl0w <lucaanthonyschwarz@googlemail.com>
2024-01-17 10:23:24 +0100

    Fix player backward movement

--- Assets/Scripts/Player/ThirdPersonPlayerController.cs
+++ Assets/Scripts/Player/ThirdPersonPlayerController.cs
@@ -39,7 +39,7 @@ public class ThirdPersonPlayerController : MonoBehaviour {
         //isRunning = Input.GetKey(KeyCode.LeftShift);
 
         // "Tank-Control" player movement
-        targetMoveDirection = ((transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"))).normalized;
+        targetMoveDirection = ((transform.forward * Mathf.Max(Input.GetAxisRaw("Vertical"), 0.0f)) + (transform.right * Input.GetAxisRaw("Horizontal"))).normalized;
         float targetSpeed = canMove && targetMoveDirection.magnitude > 0.0f ? (isRunning && GetIsGrounded() ? runSpeed : walkSpeed) : 0;
         currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, Time.deltaTime * translationDampening);
         if (targetMoveDirection.magnitude > 0.0f && canMove) { // Only rotate if the player inputs a movement direction
