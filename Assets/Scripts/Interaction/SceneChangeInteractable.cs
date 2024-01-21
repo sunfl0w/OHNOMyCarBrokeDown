9c015a606d4d5806c8a895875e45bea6a7196c32
Citrus <koschei042@gmail.com>
2024-01-21 12:37:49 +0100

    reduce transition wait time

--- Assets/Scripts/Interaction/SceneChangeInteractable.cs
+++ Assets/Scripts/Interaction/SceneChangeInteractable.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 using TMPro;
 using System;
 
-public class SceneChanceInteractable : MonoBehaviour, IInteractable {
+public class SceneChanceInteractable : MonoBehaviour, IInteractable
+{
     public string targetSceneName;
     public string targetTransformName;
     public InteractableData data;
@@ -12,46 +13,58 @@ public class SceneChanceInteractable : MonoBehaviour, IInteractable {
     public TextMeshProUGUI hintText;
 
 
-    public void Interact() {
-        if (PlayerInventory.Instance.itemExists(requiredItem) || requiredItem == null) {
-            if (data.interactSound != null) {
+    public void Interact()
+    {
+        if (PlayerInventory.Instance.itemExists(requiredItem) || requiredItem == null)
+        {
+            if (data.interactSound != null)
+            {
                 audioSource.PlayOneShot(data.interactSound);
             }
             Debug.Log("Interactable. Switch to scene: " + targetSceneName);
             SavestateManager.Instance.StoreSaveState();
             StartCoroutine(Transition());
-        } else {
+        }
+        else
+        {
             hintText.text = "The door is locked...";
             StartCoroutine(ClearHint());
         }
     }
 
-    public Transform GetTransform() {
+    public Transform GetTransform()
+    {
         return transform;
     }
 
-    public InteractableData GetData() {
+    public InteractableData GetData()
+    {
         return data;
     }
 
-    IEnumerator Transition() {
-        yield return new WaitForSeconds(3.0f);
+    IEnumerator Transition()
+    {
+        yield return new WaitForSeconds(1.0f);
         SceneTransitionManager.Instance.LoadSceneWithNextPlayerTransform(targetSceneName, targetTransformName);
     }
 
-    IEnumerator ClearHint() {
+    IEnumerator ClearHint()
+    {
         yield return new WaitForSeconds(1.0f);
         hintText.text = "";
     }
 
-    public bool CanInteract() {
+    public bool CanInteract()
+    {
         return true;
     }
 
-    public void UpdateSaveState(InteractableSaveState saveState) {
+    public void UpdateSaveState(InteractableSaveState saveState)
+    {
     }
 
-    public string GetUniqueIdentifier() {
+    public string GetUniqueIdentifier()
+    {
         return String.Empty;
     }
 }
